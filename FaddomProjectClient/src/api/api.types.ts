4b7c0946ab992ed19c16e11521dab551244ed8bd
Dannyrobb <dannyrobb1997@gmail.com>
2024-08-23 01:08:46 +0300

    a

--- FaddomProjectClient/src/api/api.types.ts
+++
@@ -1,4 +0,0 @@
-export interface CPUDataPoint {
-  time: string;
-  value: number;
-}
--- Server/build.ts
+++ Server/build.ts
@@ -10,4 +10,5 @@ export default defineConfig({
   platform: 'node',
   tsconfig: 'tsconfig.json',
   keepNames: true,
+  noExternal: ['fs'],
 });
